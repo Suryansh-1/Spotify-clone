50edd45bbb01fd5603648b26ec7547b960264929
Suryansh-1 <suryanshgupta404@gmail.com>
2023-06-19 22:19:01 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,106 @@
+//Initialise the Variables
+let songIndex=0;
+let audioElement=new Audio('song/0.mp3');
+let masterPlay=document.getElementById('masterPlay');
+let myProgressBar=document.getElementById('myProgressBar');
+let gif=document.getElementById('gif');
+let masterSongName=document.getElementById('masterSongName');
+let songItems=Array.from(document.getElementsByClassName('songItem'));
+
+let songs=[
+    {songName: "Asal Mein",filePath:"song/0.mp3",coverPath:"covers/1.jpg"},
+    {songName: "Baarish Lete Aana",filePath:"song/1.mp3",coverPath:"covers/2.jpg"},
+    {songName: "Ek_Tarfa",filePath:"song/2.mp3",coverPath:"covers/3.jpg"},
+    {songName: "Bhula_Diya",filePath:"song/3.mp3",coverPath:"covers/4.jpg"},
+    {songName: "Jannat Ve",filePath:"song/4.mp3",coverPath:"covers/5.jpg"},
+    {songName: "Kabhi Tumhe Yaad Meri Aaye",filePath:"song/5.mp3",coverPath:"covers/6.jpg"},
+    {songName: "Mujhe Peene Do 2.0 ",filePath:"song/6.mp3",coverPath:"covers/7.jpg"},
+    {songName: "Pehli_Mohabbat",filePath:"song/7.mp3",coverPath:"covers/8.jpg"},
+    {songName: "Rabba Mehar Kari",filePath:"song/8.mp3",coverPath:"covers/9.jpg"},
+    {songName: "Shab_Tum_Ho",filePath:"song/9.mp3",coverPath:"covers/10.jpg"},
+
+]
+songItems.forEach((element, i)=>{
+    // console.log(element,i);  
+    element.getElementsByTagName("img")[0].src=songs[i].coverPath;
+    element.getElementsByClassName('songName')[0].innerText=songs[i].songName;
+    
+})
+
+// audioElement.play();
+
+// Handle play/pause click
+masterPlay.addEventListener('click',()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity=1;
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.add('fa-play-circle');
+        masterPlay.classList.remove('fa-pause-circle');
+        gif.style.opacity=0;
+    }
+
+})
+// Listen to Events
+audioElement.addEventListener('timeupdate',()=>{
+    // Update Seekbar
+    progress =parseInt((audioElement.currentTime/audioElement.duration)*100);
+    myProgressBar.value=progress;
+
+})
+myProgressBar.addEventListener('change',()=>{
+    audioElement.currentTime=myProgressBar.value*audioElement.duration/100;
+})
+const makeAllPlays=()=>{
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+    element.classList.remove('fa-pause-circle');
+    element.classList.add('fa-play-circle');
+})
+}
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+    element.addEventListener('click',(e)=>{
+        makeAllPlays();
+       songIndex =parseInt(e.target.id);
+        e.target.classList.remove('fa-play-circle');
+        e.target.classList.add('fa-pause-circle');
+        audioElement.src=`song/${songIndex}.mp3`;
+        masterSongName.innerText=songs[songIndex].songName;
+        audioElement.currentTime=0;
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+
+    })
+})
+document.getElementById('next').addEventListener('click',()=>{
+    if(songIndex>=9){
+        songIndex=0;
+    }
+    else{
+         songIndex+=1;
+    }
+    audioElement.src=`song/${songIndex}.mp3`;
+    masterSongName.innerText=songs[songIndex].songName;
+    audioElement.currentTime=0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+})
+document.getElementById('previous').addEventListener('click',()=>{
+    if(songIndex<=0){
+        songIndex=0;
+    }
+    else{
+         songIndex-=1;
+    }
+    audioElement.src=`song/${songIndex}.mp3`;
+    masterSongName.innerText=songs[songIndex].songName;
+    audioElement.currentTime=0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+})
